880a1c342bb9f854ef0235d741db997da70793d5
liberbell <liberbell@gmail.com>
2018-07-06 11:48:43 +0900

    condi final

--- conditionals_start.py
+++ conditionals_start.py
@@ -3,17 +3,19 @@
 #
 
 def main():
-  x, y = 1000, 100
+  x, y = 10, 100
 
   # conditional flow uses if, elif, else
   if (x < y):
       st = "x is less than y"
+  elif (x == y):
+      st = "x is the same as y"
   else:
       st = "x is greater than y"
-
-      print(st)
+  print(st)
   # conditional statements let you use "a if C else b"
-
+  st = "x is less than y" if (x<y) else "x is greater than or same as y"
+  print(st)
 
 if __name__ == "__main__":
   main()
