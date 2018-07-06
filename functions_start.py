1586df3fae38d7b5d0358f9daf5f4ce03473828a
liberbell <liberbell@gmail.com>
2018-07-06 10:46:05 +0900

    del func1

--- functions_start.py
+++ functions_start.py
@@ -3,8 +3,8 @@
 #
 
 # define a basic function
- def func1():
-     print("I am a function")
+ # def func1():
+ #     print("I am a function")
 
 # function that takes arguments
 def func2(arg1, arg2):
