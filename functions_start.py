69a900c34694677fd168416869df27f2d0b9bd90
liberbell <liberbell@gmail.com>
2018-07-06 11:19:14 +0900

    func real final

--- functions_start.py
+++ functions_start.py
@@ -26,7 +26,7 @@ def multi_add(*args):
     result = 0
     for x in args:
         result = result + x
-        return result
+    return result
 
 func1()
 print(func1())
@@ -37,4 +37,4 @@ print(cube(3))
 print(power(2))
 print(power(2,3))
 print(power(x=3,num=2))
-print(multi_add(4,5,10,4))
+print(multi_add(2, 5, 10, 4))
