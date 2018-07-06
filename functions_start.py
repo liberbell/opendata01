75baf1ec2f5d55414eccd53d013f267ee1a0963b
liberbell <liberbell@gmail.com>
2018-07-06 11:10:33 +0900

    func final3

--- functions_start.py
+++ functions_start.py
@@ -22,6 +22,11 @@ def power(num, x=1):
     return result
 
 #function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+        return result
 
 func1()
 print(func1())
@@ -32,3 +37,4 @@ print(cube(3))
 print(power(2))
 print(power(2,3))
 print(power(x=3,num=2))
+print(multi_add(4,5,10,4))
