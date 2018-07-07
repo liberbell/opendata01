f00672364c2143a54fb13eacd9ac8972ead6bfd2
liberbell <liberbell@gmail.com>
2018-07-07 17:19:58 +0900

    loop final

--- loops_start.py
+++ loops_start.py
@@ -22,12 +22,15 @@ def main():
 #     print (d)
 
   # use the break and continue statements
-for x in range(5, 10):
-    # if (x==7):break
-    if (x % 2 == 0):continue
-    print(x)
+# for x in range(5, 10):
+#     # if (x==7):break
+#     if (x % 2 == 0):continue
+#     print(x)
 
   #using the enumerate() function to get index
+days=["Mon", "Tue", "Wed", "thu", "Fri", "Sat", "Sun"]
+for i,d in enumerate (days):
+    print (i, d)
 
 
 if __name__ == "__main__":
