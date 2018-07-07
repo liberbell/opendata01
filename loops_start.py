a38f1b138957b59f6b4b341a54a642866a690e3b
liberbell <liberbell@gmail.com>
2018-07-07 17:08:38 +0900

    loop4

--- loops_start.py
+++ loops_start.py
@@ -12,17 +12,19 @@ def main():
 
 
   # define a for loop
-  for x in range(5, 10):
-      print(x)
+  # for x in range(5, 10):
+  #     print(x)
 
 
   # use a for loop over a collection
-days=["Mon", "Tue", "Wed", "thu", "Fri", "Sat", "Sun"]
-for d in days:
-    print (d)
+# days=["Mon", "Tue", "Wed", "thu", "Fri", "Sat", "Sun"]
+# for d in days:
+#     print (d)
 
   # use the break and continue statements
-
+for x in range(5, 10):
+    if (x==7):break
+    print(x)
 
   #using the enumerate() function to get index
 
