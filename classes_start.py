48f481047165e98c5526cf37f1a5f91ea2e2373c
liberbell <liberbell@gmail.com>
2018-07-08 14:06:24 +0900

    class3

--- classes_start.py
+++ classes_start.py
@@ -8,10 +8,22 @@ class myclass():
     def method2(self, someString):
       print("myclass method2 " + someString)
 
+class anotherclass(myclass):
+    def method1(self):
+        myclass.method1(self)
+        print("anotherclass method1")
+
+    def method2(self, someString):
+        print("anotherclass method2 ")
+
 def main():
     c = myclass()
     c.method1()
     c.method2("This is a string")
+
+    c2 = anotherclass()
+    c2.method1()
+    c2.method2("This is a string")
 
 
 if __name__ == "__main__":
