dfed0d699f5b1342b4cde3c72b11a806a95fe9c7
liberbell <liberbell@gmail.com>
2018-07-08 13:58:02 +0900

    class2

--- classes_start.py
+++ classes_start.py
@@ -3,14 +3,14 @@
 #
 class myclass():
     def method1(self):
-    print("myclass method1")
+      print("myclass method1")
 
     def method2(self, someString):
-    print("myclass method2" + someString)
+      print("myclass method2 " + someString)
 
 def main():
     c = myclass()
-    c.method1
+    c.method1()
     c.method2("This is a string")
 
 
