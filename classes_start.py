450e1d14212cc74f110161fa94c326e8579255cb
liberbell <liberbell@gmail.com>
2018-07-08 13:56:11 +0900

    class1

---
+++ classes_start.py
@@ -0,0 +1,18 @@
+#
+# Example file for working with classes
+#
+class myclass():
+    def method1(self):
+    print("myclass method1")
+
+    def method2(self, someString):
+    print("myclass method2" + someString)
+
+def main():
+    c = myclass()
+    c.method1
+    c.method2("This is a string")
+
+
+if __name__ == "__main__":
+  main()
