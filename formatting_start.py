13774c896fdde2c6dd75e8b651993fd3739c6657
liberbell <liberbell@gmail.com>
2018-07-08 14:56:53 +0900

    formatting3

--- formatting_start.py
+++ formatting_start.py
@@ -12,18 +12,20 @@ def main():
   #### Date Formatting ####
 
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-  print(now.strftime("%a , %d %B , %y"))
+  # print(now.strftime("%a , %d %B , %y"))
 
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
-  print(now.strftime("Locale date and time is: %c"))
-  print(now.strftime("Locale date is: %x"))
-  print(now.strftime("Locale time is: %X"))
+  # print(now.strftime("Locale date and time is: %c"))
+  # print(now.strftime("Locale date is: %x"))
+  # print(now.strftime("Locale time is: %X"))
 
 
   #### Time Formatting ####
 
   # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+  print(now.strftime("Current time: %I:%M:%S %p"))
+  print(now.strftime("24-hour time: %H:%M"))
 
 
 
