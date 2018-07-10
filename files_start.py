08380675c513e2092591e5f546423b520220edae
liberbell <liberbell@gmail.com>
2018-07-11 07:42:13 +0900

    file3

--- files_start.py
+++ files_start.py
@@ -4,21 +4,26 @@
 
 def main():
   # Open a file for writing and create it if it doesn't exist
-  f = open("textfile.txt", "w+")
+  # f = open("textfile.txt", "w+")
 
   # Open the file for appending text to the end
+  f = open("textfile.txt", "r")
+
 
 
   # write some lines of data to the file
-  for i in range(10):
-      f.write("This is line " + str(i) +"\r\n")
+  # for i in range(10):
+  #     f.write("This is line " + str(i) +"\r\n")
 
 
   # close the file when done
-  f = f.close()
+  # f = f.close()
 
 
   # Open the file back up and read the contents
+  if f.mode == 'r':
+      contents = f.read()
+      print(contents)
 
 
 if __name__ == "__main__":
