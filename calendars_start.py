2e146aea461f384aa4e0a51c87565e229790c845
liberbell <liberbell@gmail.com>
2018-07-10 09:11:31 +0900

    calendar4

--- calendars_start.py
+++ calendars_start.py
@@ -22,13 +22,25 @@ c = calendar.TextCalendar(calendar.SUNDAY)
 
 # The Calendar module provides useful utilities for the given locale,
 # such as the names of days and months in both full and abbreviated forms
-for name in calendar.month_name:
-    print(name)
-
-for day in calendar.day_name:
-    print(day)
+# for name in calendar.month_name:
+#     print(name)
+#
+# for day in calendar.day_name:
+#     print(day)
 
 # Calculate days based on a rule: For example, consider
 # a team meeting on the first Friday of every month.
 # To figure out what days that would be for each month,
 # we can use this script:
+print("Team meeting will be on: ")
+for m in range(1,13):
+    cal = calendar.monthcalendar(2018, m)
+    weekone = cal[0]
+    weektwo = cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+
+    print("%10s %2d" % (calendar.month_name[m], meetday))
