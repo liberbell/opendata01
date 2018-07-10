ad904605b0d2f4c3f8652235431a400d51be3087
liberbell <liberbell@gmail.com>
2018-07-11 08:43:25 +0900

    os4

--- ospathutils_start.py
+++ ospathutils_start.py
@@ -10,19 +10,25 @@ import time
 
 def main():
   # Print the name of the OS
-  print(os.name)
+  # print(os.name)
 
 
   # Check for item existence and type
-  print("Items exist: " + str(path.exists("textfile.txt")))
-  print("This is a file: " + str(path.isfile("textfile.txt")))
-  print("Item is a directory: " + str(path.isdir("textfile.txt")))
+  # print("Items exist: " + str(path.exists("textfile.txt")))
+  # print("This is a file: " + str(path.isfile("textfile.txt")))
+  # print("Item is a directory: " + str(path.isdir("textfile.txt")))
 
 
   # Work with file paths
+  print("Item path: " + str(path.realpath("textfile.txt")))
+  print("Item path and name: " + str(path.split(path.realpath("textfile.txt"))))
 
 
   # Get the modification time
+  t = time.ctime(path.getmtime("textfile.txt"))
+  print(t)
+  print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
+
 
 
   # Calculate how long ago the item was modified
