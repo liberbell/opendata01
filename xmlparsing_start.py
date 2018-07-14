87b10ecd5cafb7e0bf90944cbce4132e7128f62a
liberbell <liberbell@gmail.com>
2018-07-14 17:10:19 +0900

    xml3

--- xmlparsing_start.py
+++ xmlparsing_start.py
@@ -21,6 +21,14 @@ def main():
 
 
   # create a new XML tag and add it into the document
+  newSkill = doc.createElement("skill")
+  newSkill.setAttribute("name", "jQuery")
+  doc.firstChild.appendChild(newSkill)
+
+  skills = doc.getElementsByTagName("skill")
+  print("%d skills: " % skills.length)
+  for skill in skills:
+      print(skill.getAttribute("name"))
 
 
 
