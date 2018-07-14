51a5c7fedb60c9f4e039a4293bf8b3eb33b62b59
liberbell <liberbell@gmail.com>
2018-07-14 15:14:49 +0900

    json3

--- jsondata_start.py
+++ jsondata_start.py
@@ -26,6 +26,10 @@ def printResults(data):
 
 
   # print the events that only have a magnitude greater than 4
+  for i in theJSON["features"]:
+      if i["properties"]["mag"] >= 4.0:
+        print("%2.1f" % i["properties"]["mag"], i["properties"]["place"])
+  print("---------\n")
 
 
   # print only the events where at least 1 person reported feeling something
