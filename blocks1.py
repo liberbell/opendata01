b70ea3bd34c4ae60f300257650296bf38e1c4843
liberbell <liberbell@gmail.com>
2018-07-15 17:13:31 +0900

    block11

---
+++ blocks1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 42
+y = 73
+
+print(x)
+print(y)
+if x < y:
+    z = 112
+    print('x < y: x is {} and y is {}'.format(x, y))
+    print("line2")
+    print("line3")
+    print("line4")
+
+print("z is {}".format(z))
