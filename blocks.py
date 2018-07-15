41346f11af8fee650527076219c9798e8de3c6f2
liberbell <liberbell@gmail.com>
2018-07-15 17:13:03 +0900

    block5

--- blocks.py
+++ blocks.py
@@ -7,9 +7,10 @@ y = 73
 print(x)
 print(y)
 if x < y:
+    z = 112
     print('x < y: x is {} and y is {}'.format(x, y))
     print("line2")
     print("line3")
-  print("line4")
+    print("line4")
 
-print("something else")
+print("z is {}".format(z))
