92ca00f91b4a24d0772465b649cec31027d01fd6
liberbell <liberbell@gmail.com>
2018-07-16 16:58:37 +0900

    block6

--- blocks.py
+++ blocks.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 42
-y = 73
+x = 112
+y = 42
 
-if x > y:
-    print('x > y: x is {} and y is {}'.format(x, y))
-elif x < y:
-    print('x < y: x is {} and y is {}'.format(x, y))
-elif x == y:
-    print('x = y: x is {} and y is {}'.format(x, y))
+if x == 5:
+    print("do five stuff")
+elif x == 6:
+    print("do six stuff")
+elif x == 7:
+    print("do seven stuff")
+elif x == 8:
+    print("do eight stuff")
+elif x == 42:
+    print("do fortytwo stuff")
 else:
-    print("do something else")
+    print("do that else else")
