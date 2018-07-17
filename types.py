d4132345bf7d40c60fea5bd05e3411a12cf02c0a
liberbell <liberbell@gmail.com>
2018-07-18 07:37:01 +0900

    typesbl

--- types.py
+++ types.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
-from decimal import *
 
-a = Decimal('.10')
-b = Decimal('.30')
-x = a + a + a - b
+x = True
 print('x is {}'.format(x))
 print(type(x))
