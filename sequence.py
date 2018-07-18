c0ef7279793c1e53b7834f711b8d9fd696e42f0f
liberbell <liberbell@gmail.com>
2018-07-19 07:48:33 +0900

    seq7

--- sequence.py
+++ sequence.py
@@ -2,5 +2,5 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 x = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
-for i in x:
-    print('i is {}'.format(i))
+for k, v in x.items():
+    print('K: {}, v: {}'.format(k, v))
