990c92c884d8f8820fd0657b4c8d5cacb759e122
liberbell <liberbell@gmail.com>
2018-07-19 07:46:28 +0900

    seq6

--- sequence.py
+++ sequence.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = list(range(5))
-x[2] = 42
+x = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
 for i in x:
     print('i is {}'.format(i))
