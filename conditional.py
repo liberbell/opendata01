8e210cfb716d873a9fce0f3736f49c0431c66abd
liberbell <liberbell@gmail.com>
2018-07-20 08:03:16 +0900

    cond2

--- conditional.py
+++ conditional.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-if False:
-    print('if true')
-elif False:
-    print('elif 1 true')
-elif False:
-    print('elif 2 true')
-elif True:
-    print('elif 3 true')
-elif False:
-    print('elif 4 true')
+x = 5
+
+if x == 0:
+    print('zero true')
+elif x == 1:
+    print('one 1 true')
+elif x == 2:
+    print('two 2 true')
+elif x == 3:
+    print('three 3 true')
+elif x == 4:
+    print('four 4 true')
+elif x == 5:
+    print('five 5 true')
 else:
     print('neither true')
