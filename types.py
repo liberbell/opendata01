e117e0d84a1b2cb47391b6d53e2619ab2898be18
liberbell <liberbell@gmail.com>
2018-07-20 07:55:01 +0900

    types18

--- types.py
+++ types.py
@@ -2,16 +2,18 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 x = (1, 'two', 3.0, [4, 'four'], 5)
-y = (1, 'two', 3.0, [4, 'four'], 5)
+y = [1, 'two', 3.0, [4, 'four'], 5]
 print('x is {}'.format(x))
 print(type(x))
-print(id(y))
+print(type(y))
 
 # if x is y:
 #     print('yep')
 # else:
 #     print('nope')
-if isinstance(x, tuple):
-    print('yep')
+if isinstance(y, tuple):
+    print('tuple')
+elif isinstance(y, list):
+    print('list')
 else:
     print('nope')
