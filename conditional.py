086676a0117fff6cbb61dc7cdeaf7a97a72564e8
liberbell <liberbell@gmail.com>
2018-07-20 07:59:51 +0900

    cond1

---
+++ conditional.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+if False:
+    print('if true')
+elif False:
+    print('elif 1 true')
+elif False:
+    print('elif 2 true')
+elif True:
+    print('elif 3 true')
+elif False:
+    print('elif 4 true')
+else:
+    print('neither true')
