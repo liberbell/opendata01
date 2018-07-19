e082cc348b936b351edd2739b315857d20bd6525
liberbell <liberbell@gmail.com>
2018-07-20 07:49:55 +0900

    types15

--- types.py
+++ types.py
@@ -4,5 +4,10 @@
 x = (1, 'two', 3.0, [4, 'four'], 5)
 y = (1, 'two', 3.0, [4, 'four'], 5)
 print('x is {}'.format(x))
-print(id(x[0]))
-print(id(y[0]))
+print(id(x))
+print(id(y))
+
+if x is y:
+    print('yep')
+else:
+    print('nope')
