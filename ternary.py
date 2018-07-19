03525044bb2b563ffdae5348635e7fb7ca631af2
liberbell <liberbell@gmail.com>
2018-07-20 08:30:09 +0900

    ternary2

--- ternary.py
+++ ternary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-hungry = 1
-x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
+hungry = 0
+x = 'Feed the bear now!' if hungry else None
 print(x)
