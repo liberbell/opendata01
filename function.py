9498ef1b7f36b3d96a68d307d2c711ea34d0bdfe
liberbell <liberbell@gmail.com>
2018-07-23 07:43:57 +0900

    func5

--- function.py
+++ function.py
@@ -3,21 +3,15 @@
 
 def main():
     x = [5]
-    y = x
-    y[0] = 3
-    print(id(x))
-    print(id(y))
-    print(x)
-    print(y)
-    # kitten(x)
-    # print(f"in main: x is {x}")
+    kitten(x)
+    print(f"in main: x is {x}")
     # x = kitten(5, 6, 7)
     # print(x)
 
 def kitten(a):
     # print(id(a))
-    a = 3
-    print(id(a))
+    # a = 3
+    # print(id(a))
     print('Meow.')
     print(a)
 
