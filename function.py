12beae10d884f9d68ee1bfcb149c39d18ad4856e
liberbell <liberbell@gmail.com>
2018-07-22 13:31:16 +0900

    func1

--- function.py
+++ function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-def function(n = 1):
-    print(n)
-    return n * 2
+def main():
+    kitten(5)
 
-x = function(42)
-print(x)
+def kitten(n):
+    print(f'{n} Meow.')
+
+if __name__ == '__main__': main()
