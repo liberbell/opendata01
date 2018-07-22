c07a2b64219c701636efab7258f0cb81d506669a
liberbell <liberbell@gmail.com>
2018-07-22 13:19:23 +0900

    while3

--- while.py
+++ while.py
@@ -5,11 +5,12 @@ secret = 'swordfish'
 pw = ''
 auth = False
 count = 0
-max_attempts = 3
+max_attempts = 5
 
 while pw != secret:
     count +=1
     if count > max_attempts: break
+    if count == 3:continue
     pw = input(f"{count}: What's the secret word? ")
 else:
     auth = True
