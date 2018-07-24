284df089a6f92e2bc5410f712658efe2011b6359
liberbell <liberbell@gmail.com>
2018-07-25 07:41:13 +0900

    hello4

--- hello.py
+++ hello.py
@@ -2,14 +2,18 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 # print('Hello, World.')
-def f1():
+def f1(f):
     def f2():
-        print('this is f2')
+        print('this is before the function call')
+        f()
+        print('this is after the function call')
     return f2
 
-x = f1()
-x()
+def f3():
+    print('this is f3')
 
+x = f1(f3)
+x()
 # def f1():
 #     def f2():
 #         print('this is f2')
