46c69d39dd102ae123ef38eac1574bdd3f96fd1a
liberbell <liberbell@gmail.com>
2018-07-26 07:47:05 +0900

    dict3

--- dict.py
+++ dict.py
@@ -4,13 +4,14 @@
 def main():
     animals = dict(kitten = 'meow',puppy = 'ruff!', lion= 'grrr',
         giraffe = 'I am a giraffe!', dragon = 'rawr')
-    for k, v in animals.items():
-        print(f'{k}: {v}')
+    for k in animals.keys(): print(k)
+    # for k, v in animals.items():
+    #     print(f'{k}: {v}')
     # animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
     #     'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
     # print_dict(animals)
 
 def print_dict(o):
-    for x in o: print(f'{x}: {o[x]}')
+    for k, v in o.items(): print(f'{k}: {v}')
 
 if __name__ == '__main__': main()
