56a925df45314505ca2941a35227fbd5d8c0d793
liberbell <liberbell@gmail.com>
2018-07-26 07:43:21 +0900

    dict2

--- dict.py
+++ dict.py
@@ -4,9 +4,11 @@
 def main():
     animals = dict(kitten = 'meow',puppy = 'ruff!', lion= 'grrr',
         giraffe = 'I am a giraffe!', dragon = 'rawr')
+    for k, v in animals.items():
+        print(f'{k}: {v}')
     # animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
     #     'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
-    print_dict(animals)
+    # print_dict(animals)
 
 def print_dict(o):
     for x in o: print(f'{x}: {o[x]}')
