f1253725d66a862ca55f058957fb174fa7272bc4
liberbell <liberbell@gmail.com>
2018-07-28 11:37:13 +0900

    comp5

--- comprehension.py
+++ comprehension.py
@@ -5,9 +5,9 @@ def main():
     seq = range(11)
     from math import pi
     # seq2 = [round(pi,i) for i in seq]
-    seq2 = {x: x**2 for x in seq}
+    seq2 = {x for x in 'superduper' if x not in 'pd'}
     print_list(seq)
-    print(seq2)
+    print_list(seq2)
 
 def print_list(o):
     for x in o: print(x, end = ' ')
