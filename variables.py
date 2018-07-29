0de228b4750dc041f27095979250e322175e2506
liberbell <liberbell@gmail.com>
2018-07-29 17:31:20 +0900

    vari2

--- variables.py
+++ variables.py
@@ -2,6 +2,7 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 class Animal:
+    x = [1, 2, 3]
     def __init__(self, **kwargs):
         self._type = kwargs['type'] if 'type' in kwargs else 'kitten'
         self._name = kwargs['name'] if 'name' in kwargs else 'fluffy'
@@ -28,7 +29,6 @@ def main():
     print(a0)
     print(a1)
 
-    a0._name = 'Joe'
-    print(a0._name)
+    print(a0.x)
 
 if __name__ == '__main__': main()
