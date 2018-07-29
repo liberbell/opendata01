d4eff9f9a09571b3540b4dd4c4e33def0fac5126
liberbell <liberbell@gmail.com>
2018-07-30 08:56:22 +0900

    err3

--- hello.py
+++ hello.py
@@ -3,11 +3,14 @@
 
 def main():
     try:
-        x = 5/0
+        x = 5/3
     except ValueError:
         print('I caught a ValueError')
     except ZeroDivisionError:
         print('Don\'t divide by zero')
+    else:
+        print('good job!')
+        print(x)
     # print('Hello, World.')
 
 if __name__ == '__main__': main()
