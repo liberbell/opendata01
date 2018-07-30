830eea71c8ab0f18f3f3801def5a174ac096d473
liberbell <liberbell@gmail.com>
2018-07-30 18:03:53 +0900

    hello3

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    print('Hello, World. {}'.format(42 * 7))
+    print('''
+    Hello,
+    World.
+    {}
+    '''.format(42 * 7))
 
 if __name__ == '__main__': main()
