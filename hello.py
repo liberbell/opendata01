fe476a96b0eeafb6480c3e9f91f62aa372143561
liberbell <liberbell@gmail.com>
2018-07-31 08:56:32 +0900

    hello1

--- hello.py
+++ hello.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-# def main():
-#     print('Hello,World. {}' .format(42 * 7))
-#
-# if __name__ == '__main__': main()
-class Mystring(str):
-    def __str__(self):
-        return self[::-1]
-
-s = Mystring('Hello World.')
-print(s)
+print('Hello, World.'.upper())
