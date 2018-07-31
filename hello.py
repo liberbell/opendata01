7c07719627a46ce83c03314fcf49def64d1ec8d2
liberbell <liberbell@gmail.com>
2018-08-01 08:32:22 +0900

    hello6

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 42 * 7432
+x = 42 * 7432 * 1000
 y = 73
-print('the number is {:,}'.format(x))
+print('the number is {:,}'.format(x).replace(',','.'))
