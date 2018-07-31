730e1726653b1e301a35c6ed28b7c409fc460b65
liberbell <liberbell@gmail.com>
2018-07-31 09:02:02 +0900

    hello4

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.'.swapcase())
+s1 = 'Hello, World.'
+s2 = s1.upper()
+# print('Hello, World.'.upper())
+print(id(s1))
+print(id(s2))
