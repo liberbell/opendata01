9613a619e4f4acc0b16577e031d00959e444802c
liberbell <liberbell@gmail.com>
2018-08-01 08:31:05 +0900

    hello5

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 42
+x = 42 * 7432
 y = 73
-print('the number is {0:<5} {1:+05}'.format(x, y))
+print('the number is {:,}'.format(x))
