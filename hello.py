d3922f7db43d1b062baf244d0a23dff53e663bf2
liberbell <liberbell@gmail.com>
2018-07-31 09:07:39 +0900

    hello1

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-s1 = 'Hello, World.'
-s2 = s1.upper()
-s3 = 'this is another string'
-s4 = 'this string' ' that string'
-# print('Hello, World.'.upper())
-print(id(s1))
-print(id(s2))
-print(s1 + ' ' + s3)
-print(s4)
+x = 42
+print('the number is {}'.format(x))
