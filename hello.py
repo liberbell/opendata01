0ed839d59e07f138a9f993db4fabdcc819ecb01e
liberbell <liberbell@gmail.com>
2018-08-02 08:47:36 +0900

    hello6

--- hello.py
+++ hello.py
@@ -5,7 +5,9 @@ class bunny:
     def __init__(self, n):
         self._n = n
     def __repr__(self):
-        return f'the number of bunnies is {self._n}'
+        return f'repr: the number of bunnies is {self._n}'
+    def __str__(self):
+        return f'str: the number of bunnies is {self._n}'
 
 x = bunny(47)
-print(x)
+print(str(x))
