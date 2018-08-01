0e48b008fbac2976c7af038de21890e72dd7ffaf
liberbell <liberbell@gmail.com>
2018-08-02 08:44:34 +0900

    hello2

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-s ='Hello, World.'
-print(repr(s))
+class bunny:
+    def __init__(self, n):
+        self._n = n
+    def __repr__(self):
+        return f'the number of bunnies is {self._n}'
+
+x = bunny(47)
+print(x)
