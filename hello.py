9e29ac2be4eee78692631f0b8f2707f3e4456386
liberbell <liberbell@gmail.com>
2018-08-03 07:43:47 +0900

    hello9

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 # print('Hello, World.')
-x = (1, 2, 3, 4, 5)
-y =min(x)
+# x = (1, 2, 3, 4, 5)
+x = (0, 0, 0, 0, 0)
+y = any(x)
 # y = reversed(x)
 # for i in y:
 #     print(i)
