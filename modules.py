e00bbcf61a67cb8ec4f00212bb1c7123bf51f29e
liberbell <liberbell@gmail.com>
2018-08-04 15:02:22 +0900

    modules1

---
+++ modules.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+import sys
+
+def main():
+    # v = sys.version_info
+    v = sys.platform
+    # print('Python version {}.{}.{}'.format(*v))
+    print(v)
+
+if __name__ == '__main__': main()
