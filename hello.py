a8c3e0121b4520c0af4fd4b1df41cebae99512e5
liberbell <liberbell@gmail.com>
2018-08-04 14:54:30 +0900

    hello9

--- hello.py
+++ hello.py
@@ -2,15 +2,17 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 # print('Hello, World.')
-x = (1, 2, 3, 4, 5)
+# x = (1, 2, 3, 4, 5)
 # x = (0, 0, 1, 0, 0)
-y = (6, 7, 8, 9)
-z = zip(x,y)
+# y = (6, 7, 8, 9)
+# z = zip(x,y)
 # y = reversed(x)
 # for i in y:
 #     print(i)
 # print(x)
 # print(y)
 
-for a, b in z:
-    print(f'{a}, - {b}')
+# for a, b in z:
+    # print(f'{a}, - {b}')
+x = ('cat', 'dog', 'rabbit', 'velociraptor')
+for i, v in enumerate(x): print(f'{i}: {v}')
