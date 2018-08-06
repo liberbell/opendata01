fcaf90f7bf18c9d7df71804044fe590b18488cb2
liberbell <liberbell@gmail.com>
2018-08-06 17:18:04 +0900

    tk2

--- 05_hello_tkinter.py
+++ 05_hello_tkinter.py
@@ -3,7 +3,10 @@
 # This is an exercise file from Python GUI Development with Tkinter on lynda.com
 
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
+button = ttk.Button(root, text = 'click Me')
+button.pack()
 Label(root, text="Hello, Tkinter!").pack()
 root.mainloop()
