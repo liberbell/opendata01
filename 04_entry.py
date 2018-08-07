28810fe7a0a5b4c7accd8894cef50a84ace3544f
liberbell <liberbell@gmail.com>
2018-08-08 07:41:07 +0900

    ent2

--- 04_entry.py
+++ 04_entry.py
@@ -3,8 +3,8 @@
 # This is an exercise file from Python GUI Development with Tkinter on lynda.com
 
 from tkinter import *
-from tkinter import ttk        
-    
+from tkinter import ttk
+
 root = Tk()
 
 entry = ttk.Entry(root, width = 30)
@@ -12,12 +12,12 @@ entry.pack()
 
 entry.get()
 entry.delete(0, 1)
-entry.delete(0, END)
-entry.insert(0, 'Enter your password')
-
-entry.config(show = '*')
-entry.state(['disabled'])
-entry.state(['readonly'])
-entry.state(['!disabled'])
+# entry.delete(0, END)
+# entry.insert(0, 'Enter your password')
+#
+# entry.config(show = '*')
+# entry.state(['disabled'])
+# entry.state(['readonly'])
+# entry.state(['!disabled'])
 
 root.mainloop()
