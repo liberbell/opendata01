b829ba23d69bafc97a4c70a5bee1ae2428974522
liberbell <liberbell@gmail.com>
2018-08-09 08:35:54 +0900

    eq5

--- 01_02_Start.py
+++ 01_02_Start.py
@@ -36,6 +36,7 @@ print(False > True)
 # Looking for first mismatched letter
 # A - Z --> 1 - 26
 # > --> is greater than
+print('Jennifer' > 'Jenny')
 
 # A - Z --> 1 - 26
 # <= --> is less than or equal to
