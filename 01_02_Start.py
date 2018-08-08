8511406cd8880f39a3bb3bf774f8d9a191cafe09
liberbell <liberbell@gmail.com>
2018-08-09 08:28:50 +0900

    eq1

---
+++ 01_02_Start.py
@@ -0,0 +1,37 @@
+# Python Comparison Operators
+
+# TIPS:
+# == --> is equal to
+# <= --> is less than or equal to
+# >= --> is greater than or equal to
+# < --> is less than
+# > --> is greater than
+
+
+
+# < --> is less than
+print(10<75)
+print(75<10)
+
+if 10 < 75:
+    print('The bigger number is bigger')
+
+# == --> is equal to
+kitten = 10
+tiger = 75
+
+if kitten < tiger:
+    print()
+
+# < --> is less than
+
+#False --> 0
+#True --> 1
+# > --> is greater than
+
+# Looking for first mismatched letter
+# A - Z --> 1 - 26
+# > --> is greater than
+
+# A - Z --> 1 - 26
+# <= --> is less than or equal to
