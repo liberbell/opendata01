ccd7231775741534e03f2215b6458f9d25782554
liberbell <liberbell@gmail.com>
2018-08-10 08:56:49 +0900

    range2

--- 01_04_Start.py
+++ 01_04_Start.py
@@ -1,2 +1,9 @@
 # Range -> range instance that holds all nums counting by one between 0 and first input
 # List -> lists numbers from the inputted tuple
+
+numberdContestants = range(30)
+
+print(list(numberdContestants))
+
+for c in list(numberdContestants):
+    print('Contestant ' + str(c) + ' is here')
