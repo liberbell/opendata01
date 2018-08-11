2479e6b6e43f6a5633ba0bcc7c826152534b10bd
liberbell <liberbell@gmail.com>
2018-08-11 21:28:07 +0900

    sort4

--- 01_07_Start.py
+++ 01_07_Start.py
@@ -11,3 +11,6 @@ print(sorted(['Sue', 'jerry', 'linda']))
 # Key Parameters
 print(sorted('My favorite child is Linda'.split(), key=str.upper))
 print(sorted(pointsInaGame,reverse=True))
+
+leaderboard = {231: 'CKL',123: 'abc', 432:'JKC'}
+print(sorted(leaderboard,reverse=True))
