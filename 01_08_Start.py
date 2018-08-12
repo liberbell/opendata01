ac92ce497168b7e05e30453d35cbb7335340302a
liberbell <liberbell@gmail.com>
2018-08-12 20:47:50 +0900

    type5

--- 01_08_Start.py
+++ 01_08_Start.py
@@ -3,3 +3,15 @@ print(type(r))
 print(type(10))
 print(type('a'))
 print(type('Hi there'))
+
+class cars:
+    pass
+
+class Trucks(cars):
+    pass
+
+c = cars()
+t = Trucks()
+
+print(type(c))
+print(type(t))
