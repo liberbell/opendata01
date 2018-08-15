2caee2a526448decd9900dd605d6728fefc6037c
liberbell <liberbell@gmail.com>
2018-08-15 13:03:50 +0900

    iter1

---
+++ 02_06_Start.py
@@ -0,0 +1,13 @@
+# Itertools Part 2
+import itertools
+
+election = {1: 'Barb', 2: 'Karen', 3: 'Erin'}
+for p in itertools.permutations(election):
+    print(p)
+
+for p1 in itertools.permutations(election.values()):
+    print(p1)
+
+# Permutations: Order matters - some copies with same inputs but in different order
+
+# Combinations: Order does not matter - no copies with same inputs
