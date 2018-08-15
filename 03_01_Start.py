597346e71c2cf89f21b7c1a849b63c3761af8fc4
liberbell <liberbell@gmail.com>
2018-08-16 07:45:17 +0900

    sys2

--- 03_01_Start.py
+++ 03_01_Start.py
@@ -6,5 +6,8 @@ print('Number of Arguments:', len(sys.argv), ' arguments')
 print('Arguments ', sys.argv)
 
 # Remove Arguments
+sys.argv.remove(sys.argv[0])
+
+print('Arguments', sys.argv)
 
 # Sum up the Arguments
