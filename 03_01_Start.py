cb7e9ac3e8bc3653e6267667187ee830e668e807
liberbell <liberbell@gmail.com>
2018-08-16 07:48:57 +0900

    sys3

--- 03_01_Start.py
+++ 03_01_Start.py
@@ -11,3 +11,14 @@ sys.argv.remove(sys.argv[0])
 print('Arguments', sys.argv)
 
 # Sum up the Arguments
+arguments = sys.argv
+sum = 0
+for arg in arguments:
+    try:
+        number = int(arg)
+        sum = sum + number
+
+    except Exception:
+        print('Bad input')
+
+print(sum) 
