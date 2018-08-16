45f39d8c40971defd8ed8903831cf97474bcdd61
liberbell <liberbell@gmail.com>
2018-08-17 08:50:15 +0900

    file9

--- 03_05_Start.py
+++ 03_05_Start.py
@@ -3,5 +3,10 @@ myFile = open("scores.txt", "r")
 
 # Read one line at a time
 print('My one line ' + myFile.readline())
+myFile.seek(0)
+
 
 # Iterate through each line of a file
+for line in myFile:
+    newHighScore = line.replace('BBB', 'PDJ')
+    print(newHighScore)
