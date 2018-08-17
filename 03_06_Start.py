ce423fe581b12029f387058419a4ddf7b576a914
liberbell <liberbell@gmail.com>
2018-08-17 09:00:08 +0900

    temp2

--- 03_06_Start.py
+++ 03_06_Start.py
@@ -1,9 +1,15 @@
 # Tempfile Module
+import tempfile
 
 # Create a temporary file
+tempFile = tempfile.TemporaryFile()
 
 # Write to a temporary file
+tempFile.write(b'save this special number for me 5678990')
+tempFile.seek(0)
 
 # Read the temporary file
+print(tempFile.read())
 
-# Close the temporary file
+# Close the temporary
+tempFile.close()
