59030364a7f292032c9673948dd4be1c544e542e
liberbell <liberbell@gmail.com>
2018-08-18 11:18:18 +0900

    zip1

---
+++ 03_07_Start.py
@@ -0,0 +1,14 @@
+# Zipfile Module
+import zipfile
+
+# Open and List
+zip = zipfile.ZipFile('archives.zip', 'r')
+print(zip.namelist())
+
+# Metadata in the zip folder
+
+# Access to files in zip folder
+
+# Extracting files
+
+# Closing the zip
