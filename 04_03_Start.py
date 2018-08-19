d73a1e505b930f2892340d04684d3a90c5a1f62a
liberbell <liberbell@gmail.com>
2018-08-19 15:43:49 +0900

    timecal4

--- 04_03_Start.py
+++ 04_03_Start.py
@@ -6,5 +6,6 @@ now = datetime.now()
 testDate = now + timedelta(days=2)
 myFirstLinkedInCourse = now - timedelta(weeks=3)
 
+print(now.date())
 print(testDate.date())
 print(myFirstLinkedInCourse.date())
