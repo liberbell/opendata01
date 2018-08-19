cd80d356be7b09cf48e0abec985aa0b616ce551b
liberbell <liberbell@gmail.com>
2018-08-19 16:14:57 +0900

    pars1

---
+++ 04_05_Start.py
@@ -0,0 +1,19 @@
+# HTML Parser Module
+from html.parser import HTMLParser
+
+class HTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attr):
+        print('Start tag: ', tag)
+        for attr in attr:
+            print('attr: ', attr)
+
+    def handle_endtag(self, tag):
+        print('End tag: ', tag)
+
+    def handle_comments(self, data):
+        print('Comments: ', data)
+    def handle_data(self, data):
+        print('Data: ', data)
+
+parser = HTMLParser()
+parser.feed('<html><head><code></title></head><body><h1><!--hi-->I am a code</h1></body></html>')
