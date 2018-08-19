690457324b8c9d368a7d2cebeda33715379f6bbb
liberbell <liberbell@gmail.com>
2018-08-20 07:53:00 +0900

    wrap1

---
+++ 04_06_Start.py
@@ -0,0 +1,20 @@
+# Text Wrap Module
+import textwrap
+
+websiteText = """   Learning can happen anywhere with our apps on your computer,
+mobile device, and TV, featuring enhanced navigation and faster streaming
+for anytime learning. Limitless learning, limitless possibilities."""
+
+print("No Dedent:")
+print(textwrap.fill(websiteText))
+
+print("Dedent")
+dedent_text = textwrap.dedent(websiteText).strip()
+print(dedent_text)
+
+print("Fill")
+print(textwrap.fill(dedent_text))
+
+print("Controlling Indent")
+
+print("Shortening Text")
