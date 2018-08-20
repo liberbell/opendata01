c7737978e28a701f45407d36497a22354904fbd7
liberbell <liberbell@gmail.com>
2018-08-21 08:59:36 +0900

    url3

--- 04_07_Start.py
+++ 04_07_Start.py
@@ -9,3 +9,8 @@ with urllib.request.urlopen('https://www.googleapis.com/books/v1/volumes?q=isbn:
     text = f.read()
     decodetext = text.decode('utf-8')
     print(textwrap.fill(decodetext, width=50))
+
+print()
+
+obj = json.loads(decodetext)
+print(obj['kind'])
